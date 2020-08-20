fd73ba5214a21e571837a481c97c5e326e26fc5e
Tim Penhey <tim@penhey.net>
2020-08-21 10:32:56 +1200

    Add deep equals and others.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/howbazaar/checkers
+
+go 1.14
--- suite.go
+++ suite.go
@@ -110,6 +110,25 @@ func (equals) Check(t *testing.T, obtained interface{}, extras ...interface{}) b
 	return false
 }
 
+type deepEquals struct{}
+
+// DeepEquals checker tests for equality of complex types.
+var DeepEquals Checker = deepEquals{}
+
+func (deepEquals) Check(t *testing.T, obtained interface{}, extras ...interface{}) bool {
+	if len(extras) == 0 {
+		t.Errorf("missing 'expected' value")
+		return false
+	}
+	expected, extras := extras[0], extras[1:]
+
+	if ok, err := DeepEqual(obtained, expected); !ok {
+		t.Error(err.Error())
+		return false
+	}
+	return true
+}
+
 type isFalse struct{}
 
 var IsFalse Checker = isFalse{}
@@ -146,4 +165,35 @@ func (isTrue) Check(t *testing.T, obtained interface{}, extras ...interface{}) b
 	}
 	t.Error("obtained value is false")
 	return false
+}
+
+type hasLen struct{}
+
+var HasLen Checker = hasLen{}
+
+func (hasLen) Check(t *testing.T, obtained interface{}, extras ...interface{}) bool {
+	if len(extras) == 0 {
+		t.Errorf("missing 'expected' value")
+		return false
+	}
+	expected, extras := extras[0], extras[1:]
+	// TODO: deal with panics of wrong types and len of unexpected types
+	size := reflect.ValueOf(expected).Int()
+
+	value := reflect.ValueOf(obtained)
+	var length int
+	switch value.Kind() {
+	case reflect.Array, reflect.Chan, reflect.Map, reflect.Slice, reflect.String:
+		length = value.Len()
+	default:
+		t.Errorf("HasLen checker expected array, channel, map, slice or string, obtained was type %T", obtained)
+		return false
+	}
+
+	if int64(length) != size {
+		t.Errorf("expected length %d, obtained %d", size, length)
+		return false
+	}
+
+	return true
 }
